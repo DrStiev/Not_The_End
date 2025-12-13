683001f670855bf91d81d4d06cabcce4fe534054
DrStiev <mstievano9@gmail.com>
2025-12-13 11:38:04 +0100

    handle '\n' char in popup

--- src/ui.rs
+++ src/ui.rs
@@ -985,7 +985,7 @@ fn draw_node_edit_popup(f: &mut Frame, app: &App) {
     let area = centered_rect(50, 20, f.area());
 
     let popup_block = Block::default()
-        .title(Line::from("Modifica (Esc per confermare)").alignment(Alignment::Center))
+        .title(Line::from(" Modifica (Esc per confermare) ").alignment(Alignment::Center))
         .borders(Borders::ALL)
         .border_type(BorderType::Rounded)
         .style(Style::default().bg(Color::Black).fg(Color::Yellow));
@@ -1013,21 +1013,33 @@ fn draw_list_edit_popup(f: &mut Frame, app: &App) {
     let area = centered_rect(70, 30, f.area());
 
     let popup_block = Block::default()
-        .title(Line::from("Modifica (Esc per confermare)").alignment(Alignment::Center))
+        .title(Line::from(" Modifica (Esc per confermare) ").alignment(Alignment::Center))
         .borders(Borders::ALL)
         .border_type(BorderType::Rounded)
         .style(Style::default().bg(Color::Black).fg(Color::Yellow));
 
-    let text = vec![
-        Line::from(""),
-        Line::from(vec![
-            Span::styled(
-                &app.list_edit_buffer,
-                Style::default().add_modifier(Modifier::BOLD),
-            ),
-            Span::styled("▌", Style::default().fg(Color::LightYellow)),
-        ]),
-    ];
+    let mut text = vec![Line::from("")];
+
+    // handle \n character
+    let temp = &app.list_edit_buffer;
+    let mut curr = 0;
+    for (i, c) in temp.chars().enumerate() {
+        if c == '\n' {
+            if curr == i {
+                text.push(Line::from(""));
+            } else {
+                text.push(Line::from(Span::styled(
+                    &temp[curr..i],
+                    Style::default().add_modifier(Modifier::BOLD),
+                )));
+            }
+            curr = i + 1;
+        }
+    }
+    text.push(Line::from(vec![
+        Span::styled(&temp[curr..], Style::default().add_modifier(Modifier::BOLD)),
+        Span::styled("▌", Style::default().fg(Color::LightYellow)),
+    ]));
 
     let paragraph = Paragraph::new(text)
         .block(popup_block)
